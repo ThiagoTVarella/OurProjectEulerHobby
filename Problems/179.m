a20e131d183a924e2b0caecb89709acc670b9c4c
anzae <55374608+anzae@users.noreply.github.com>
2021-05-09 12:49:35 -0300

    Create 179.m

---
+++ Problems/179.m
@@ -0,0 +1,21 @@
+% Find the number of integers 1 < n < 107, for which n and n + 1 have the same number of positive divisors. 
+%  For example, 14 has the positive divisors 1, 2, 7, 14 while 15 has 1, 3, 5, 15.
+
+N = 1e5;
+nant = 1;
+cont = 0;
+ni = 0;
+n_last = 1;
+for n = 2:N-1
+    facs = factor(n);
+    divs = [1,facs(1)];
+    for fi = facs(2:end)
+        divs = [1;fi]*divs;
+        divs = divs(:)';
+    end
+    ni = length(unique(divs));
+    if ni == n_last
+        cont = cont+1;
+    end
+    n_last = ni;
+end
